eb1c16c2d99bcb12d6bfe040b584d5179cdb3dad
YJohn <yfekadu2016@gmail.com>
2017-09-04 11:38:19 +0100

    created add-post.js file

---
+++ public/js/add-post.js
@@ -0,0 +1,34 @@
+var addPostButton = document.querySelector('#addPostButton');
+addPostButton.addEventListener('click', function() {
+
+    const title = document.getElementById("title").value;
+    const summary = document.getElementById("summary").value;
+    const contents = document.getElementById("contents").value;
+
+    // create data object
+    const postData= {
+        title: title,
+        summary: summary,
+        contents: contents
+    }
+
+    // AJAX
+    var url = '/admin';
+    var oReq = new XMLHttpRequest();
+
+    oReq.addEventListener('load', onLoad);
+    oReq.open('POST', url);
+    //Send the proper header information along with the request
+    oReq.setRequestHeader("Content-type", "application/json");
+    oReq.send(JSON.stringify(postData));
+});
+
+function onLoad() {
+    // clear form 
+    document.getElementById("title").value = "";
+    document.getElementById("summary").value = "";
+    document.getElementById("contents").value = "";
+
+    // redirect to main page
+    window.location.href = '/';
+}
--- server.js
+++ server.js
@@ -2,43 +2,85 @@ const express = require('express');
 const app = express();
 const exphbs  = require('express-handlebars');
 const fs = require('fs');
+var bodyParser = require('body-parser')
+
+// Load the SDK for JavaScript
+var AWS = require('aws-sdk');
+
+
 
 // Then these two lines after you initialise your express app 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
+// Load credentials and set region from JSON file
+AWS.config.loadFromPath('./config.json');
+
+
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(middleware);
-function bodymiddleware(req,res, next){
-    req.body={}
-    next()
-}
-
-app.use(middleware);
-app.use(function(req,res, next){
-    console.log(req.body);
-})
 // The extensions 'html' allows us to serve file without adding .html at the end 
 // i.e /my-cv will server /my-cv.html
+
+
 app.use(express.static("public", {'extensions': ['html']}));
 
 app.get('/', function (req, res) {
-    const filePath = __dirname + '/data/posts.json';
-    const callbackFunction = function(error, file) {
-        // we call .toString() to turn the file buffer to a String
-        const fileData = file.toString();
-        // we use JSON.parse to get an object out the String
-        const postsJson = JSON.parse(fileData);
-        // send the json to the Template to render
-        res.render('index', {
-          title: "Yohannes's profile",
-          subheading: "A modern Website built in Node with Handlebars",
-          posts: postsJson
-        });
+    var docClient = new AWS.DynamoDB.DocumentClient();
+
+    var params = {
+        TableName: "cyf-yfekadu-posts",
+        ProjectionExpression: "title, summary, content",
     };
-    fs.readFile(filePath, callbackFunction);
+
+    console.log("Scanning posts table.");
+    docClient.scan(params, onScan);
+
+    function onScan(err, data) {
+        if (err) {
+            console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            // print all the posts
+            console.log("Scan succeeded.");
+            data.Items.forEach(function(post) {
+            console.log(
+                    post.title + ": ",
+                    post.summary, "- content:", post.content);
+            });
+
+            res.render('index', {
+                title: "yohannes's profile",
+                subheading: "A modern Website built in Node with Handlebars",
+                posts: data.Items
+            });
+        }
+    }
+});
+
+app.post('/admin', function (req, res) {
+
+    // Create the DynamoDB service object
+    var docClient = new AWS.DynamoDB.DocumentClient();
+
+    var params = {
+        TableName: 'cyf-yfekadu-posts',
+        Item:{
+            'title' : req.body.title,
+            'summary' : req.body.summary,
+            'content' : req.body.contents,
+        }
+    };
+
+    // Call DynamoDB to add the item to the table
+    console.log("Adding a new item...");
+    docClient.put(params, function(err, data) {
+        if (err) {
+            console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("Added item:", JSON.stringify(data, null, 2));
+        }
+    });
+res.end();
 });
 
 app.get('/api/posts', function (req, res) {
